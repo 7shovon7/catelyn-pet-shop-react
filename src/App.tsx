76538c5e52629abe88ce2d7cea19796c20325615
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-05-26 12:08:31 +0600

    flexible layout designed

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,62 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Show } from "@chakra-ui/react";
+import Logo from "./components/Logo";
+import pawPattern from "./assets/paw-pattern.svg";
 
 function App() {
     return (
         <Grid
             templateAreas={{
-                base: `"logo nav" "main main"`,
-                lg: `"logo nav account" "main main main"`,
+                base: `"header header" "main main"`,
+                lg: `"header header header" "main main main"`,
             }}
+            templateColumns={{ base: "1fr 1fr", lg: "1fr 1fr 1fr" }}
+            templateRows={{ base: "auto auto", lg: "auto auto" }}
+            // gap={4}
+            // p={4}
         >
-            <GridItem area="logo" bg="coral">
-                Logo
-            </GridItem>
-            <GridItem area="nav" bg="green">
-                Nav
-                {/* <Show below="lg">
-                    <GridItem area="account" bg="blue">
-                        Cart
+            <Box
+                gridArea="header"
+                bgImage={pawPattern}
+                bgRepeat="no-repeat"
+                bgSize="cover"
+                bgPosition="center"
+                backgroundColor="#17383E"
+                backgroundBlendMode="overlay"
+                display="flex"
+                flexDirection={{ base: "column", lg: "row" }}
+                w="100%"
+                h={81}
+            >
+                <Grid
+                    templateAreas={{
+                        base: `"logo nav"`,
+                        lg: `"logo nav account"`,
+                    }}
+                    templateColumns={{
+                        base: "350px 1fr",
+                        md: "350px 1fr 1fr",
+                        lg: "450px 1fr 1fr",
+                    }}
+                    alignItems="center"
+                    w="100%"
+                >
+                    <GridItem area="logo">
+                        <Logo />
                     </GridItem>
-                </Show> */}
-            </GridItem>
-            <Show above="lg">
-                <GridItem area="account" bg="blue">
-                    Cart
-                </GridItem>
-            </Show>
+                    <GridItem area="nav" display="flex" justifyContent="center">
+                        Nav
+                    </GridItem>
+                    <Show above="lg">
+                        <GridItem
+                            area="account"
+                            display="flex"
+                            justifyContent="flex-end"
+                        >
+                            Cart
+                        </GridItem>
+                    </Show>
+                </Grid>
+            </Box>
             <GridItem area="main" bg="dodgerblue">
                 Main
             </GridItem>
---
+++ src/components/Logo.tsx
@@ -0,0 +1,8 @@
+import { Image } from "@chakra-ui/react";
+import logo from "../assets/logo.png";
+
+const Logo = () => {
+    return <Image src={logo} height={7} margin={4} />;
+};
+
+export default Logo;
