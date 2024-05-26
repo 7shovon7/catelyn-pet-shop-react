ead03ca26a7826e2606b60ca80e48fa347acd14a
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-05-26 11:20:08 +0600

    basic layout rendered

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-    return <Button colorScheme="blue">A button</Button>;
+    return (
+        <Grid
+            templateAreas={{
+                base: `"logo nav" "main main"`,
+                lg: `"logo nav account" "main main main"`,
+            }}
+        >
+            <GridItem area="logo" bg="coral">
+                Logo
+            </GridItem>
+            <GridItem area="nav" bg="green">
+                Nav
+                {/* <Show below="lg">
+                    <GridItem area="account" bg="blue">
+                        Cart
+                    </GridItem>
+                </Show> */}
+            </GridItem>
+            <Show above="lg">
+                <GridItem area="account" bg="blue">
+                    Cart
+                </GridItem>
+            </Show>
+            <GridItem area="main" bg="dodgerblue">
+                Main
+            </GridItem>
+        </Grid>
+    );
 }
 
 export default App;
