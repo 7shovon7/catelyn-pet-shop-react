9dd011f0bb8b967bbf7564523844d30880108654
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-06-03 23:47:00 +0600

    Blog grid is functional

---
+++ src/components/Blogs/BlogGrid.tsx
@@ -0,0 +1,127 @@
+import { Flex, HStack, SimpleGrid, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import BlogCard from "./BlogCard";
+import CButton from "../Regular/CButton";
+import earMitesImage from "../../../assets/ear-mites.jpg";
+
+const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
+
+export interface BlogPost {
+    id: number;
+    title: string;
+    content: string;
+    created_at: string;
+    updated_at: string;
+}
+
+export interface BlogPostResponse {
+    count: number;
+    next: string | null;
+    previous: string | null;
+    results: BlogPost[];
+}
+
+export interface BlogPostResponseUI {
+    count: number;
+    resultsLength: number;
+}
+
+interface AllBlogsProps {
+    limit?: number;
+    onDataFetched?: (data: BlogPostResponseUI) => void;
+    pagination?: boolean;
+}
+
+const BlogGrid: React.FC<AllBlogsProps> = ({
+    limit = 10,
+    onDataFetched,
+    pagination = false,
+}: AllBlogsProps) => {
+    const [blogs, setBlogs] = useState<BlogPost[]>([]);
+    const [page, setPage] = useState<number>(1);
+    const [count, setCount] = useState<number | null>(null);
+
+    useEffect(() => {
+        const fetchBlogs = async () => {
+            try {
+                const offset = pagination ? (page - 1) * limit : 0;
+                const response = await axios.get(
+                    `${BASE_API_URL}/blog/posts/?limit=${limit}&offset=${offset}`
+                );
+                const data: BlogPostResponse = response.data;
+                console.log(data);
+                setBlogs(data.results);
+                setCount(data.count);
+                if (onDataFetched) {
+                    onDataFetched({
+                        count: data.count,
+                        resultsLength: data.results.length,
+                    });
+                }
+            } catch (error) {
+                console.error("Error fetching blog posts:", error);
+            }
+        };
+
+        fetchBlogs();
+    }, [limit, page, onDataFetched, pagination]);
+
+    const totalPages = count ? Math.ceil(count / limit) : 1;
+
+    const handlePreviousPage = () => {
+        setPage((prevPage) => Math.max(prevPage - 1, 1));
+    };
+
+    const handleNextPage = () => {
+        setPage((prevPage) => Math.min(prevPage + 1, totalPages));
+    };
+
+    return (
+        <>
+            <SimpleGrid
+                columns={{ base: 1, sm: 2, md: 2, lg: 3, xl: 4 }}
+                spacing={8}
+                marginY={4}
+            >
+                {blogs.map((blog) => {
+                    const imageUrlMatch = blog.content.match(/!\[\]\((.*?)\)/);
+                    const imageUrl = imageUrlMatch
+                        ? `${BASE_API_URL}${imageUrlMatch[1]}`
+                        : earMitesImage;
+                    return (
+                        <BlogCard
+                            key={blog.id}
+                            title={blog.title}
+                            imageSrc={imageUrl}
+                            postedAt={new Date(
+                                blog.created_at
+                            ).toLocaleDateString()}
+                        />
+                    );
+                })}
+            </SimpleGrid>
+            {pagination && (
+                <Flex justifyContent="center" marginTop="32px">
+                    <HStack spacing={4}>
+                        <CButton
+                            onClick={handlePreviousPage}
+                            disabled={page === 1}
+                        >
+                            Previous
+                        </CButton>
+                        <Text>{page}</Text>
+                        <CButton
+                            onClick={handleNextPage}
+                            disabled={page === totalPages}
+                        >
+                            Next
+                        </CButton>
+                    </HStack>
+                </Flex>
+            )}
+        </>
+    );
+};
+
+export default BlogGrid;
--- src/pages/Home/AllBlogs/index.tsx
+++ src/pages/Home/AllBlogs/index.tsx
@@ -1,38 +1,11 @@
-import { SimpleGrid } from "@chakra-ui/react";
 import SectionHeader from "../SectionHeader";
-import BlogCard from "../../../components/Regular/BlogCard";
-import earMitesImage from "../../../assets/ear-mites.jpg";
+import BlogGrid from "../../../components/Blogs/BlogGrid";
 
 const AllBlogs = () => {
     return (
         <>
             <SectionHeader title="Pet Blogs" />
-            <SimpleGrid
-                columns={{ base: 1, sm: 2, md: 2, lg: 3, xl: 4 }}
-                spacing={8}
-                marginY={4}
-            >
-                <BlogCard
-                    title="বিড়ালের কানের ময়লা পরিষ্কার করুন"
-                    imageSrc={earMitesImage}
-                    postedAt="January 12, 2024"
-                />
-                <BlogCard
-                    title="বিড়ালের কানের ময়লা পরিষ্কার করুন"
-                    imageSrc={earMitesImage}
-                    postedAt="March 23, 2024"
-                />
-                <BlogCard
-                    title="বিড়ালের কানের ময়লা পরিষ্কার করুন"
-                    imageSrc={earMitesImage}
-                    postedAt="April 07, 2024"
-                />
-                <BlogCard
-                    title="বিড়ালের কানের ময়লা পরিষ্কার করুন"
-                    imageSrc={earMitesImage}
-                    postedAt="May 08, 2024"
-                />
-            </SimpleGrid>
+            <BlogGrid />
         </>
     );
 };
