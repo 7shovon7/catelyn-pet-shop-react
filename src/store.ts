493ba6bbae835af9788d9239685728bbdb909eb8
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-08-30 09:28:32 +0600

    comments cleared

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,6 @@
-// src/store.ts
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "features/auth/slice";
 import categoryReducer from "features/product/slices/category";
-// import categoryReducer from "components/Category/categorySlice";
-// import cartReducer from "components/Order/cartSlice";
-// import { loadState, saveState } from "utils/localStorage";
 
 export const store = configureStore({
     reducer: {
